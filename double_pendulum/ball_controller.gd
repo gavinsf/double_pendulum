590cdec00458790cf77d60bc708615ae237b0063
NamyaKamboj <152056909+NamyaKamboj@users.noreply.github.com>
2024-11-09 15:13:27 -0800

    Update ball_controller.gd

--- double_pendulum/ball_controller.gd
+++ double_pendulum/ball_controller.gd
@@ -1,48 +1,49 @@
 extends Node3D
 
-var accel_1_x : float = 0
-var accel_1_y : float = 0
-var accel_2_x : float = 0
-var accel_2_y : float = 0
-var vel_1_x : float = 0
-var vel_1_y : float = 0
-var vel_2_x : float = 0
-var vel_2_y : float = 0
-var angle_1 : float
-var angle_2 : float
-var rope_length : float = 1
-var x_1 : float
-var y_1 : float
-var x_2 : float
-var y_2 : float
-var initial_angle : float
-var prev_pos : Vector3
+var angle_1 : float = 0.0
+var angle_2 : float = 0.0
+var rope_length_1 : float = 1.0  # Length of the first rope
+var rope_length_2 : float = 1.0  # Length of the second rope
+var gravity : float = 9.81
+var angular_velocity_1 : float = 0.0
+var angular_velocity_2 : float = 0.0
+var initial_angle_1 : float = PI / 3  # Starting angle for upper object (60 degrees)
+var initial_angle_2 : float = PI / 3  # Starting angle for lower object (60 degrees)
 
 @onready var upper_ball = $"Upper Ball"
 @onready var lower_ball = $"Lower Ball"
 @onready var anchor = $"Anchor"
 
-
 func _ready() -> void:
-	pass 
+	angle_1 = initial_angle_1  # initial angle for the upper object
+	angle_2 = initial_angle_2  # initial angle for the lower object
 
 func _physics_process(delta: float) -> void:
-	x_1 = upper_ball.position.x
-	y_1 = upper_ball.position.y
-	x_2 = lower_ball.position.x
-	y_2 = lower_ball.position.y
+	# angular acceleration
+	var angular_acceleration_1 = (-gravity / rope_length_1) * sin(angle_1)
+	var angular_acceleration_2 = (-gravity / rope_length_2) * sin(angle_2)
 	
-	angle_1 = asin(x_1/rope_length)
+	# Update angular velocities
+	angular_velocity_1 += angular_acceleration_1 * delta
+	angular_velocity_2 += angular_acceleration_2 * delta
 	
-	accel_1_x = -9.81 * sin(angle_1)
-	accel_1_y = -9.81 * cos(angle_1)
+	# Updating angles
+	angle_1 += angular_velocity_1 * delta
+	angle_2 += angular_velocity_2 * delta
 	
-	vel_1_x += accel_1_x / 60
-	vel_1_y += accel_1_y / 60
+	# position of the upper object
+	var x_1 = anchor.position.x + rope_length_1 * sin(angle_1)
+	var y_1 = anchor.position.y + rope_length_1 * cos(angle_1)
 	
-	prev_pos = upper_ball.position
-	upper_ball.position = prev_pos + Vector3(vel_1_x/60, vel_1_y/60, 0)
+	# Update position of the upper
+	upper_ball.position = Vector3(x_1, y_1, 0)
 	
-	print(x_1, accel_1_y, "		", angle_1)
-	pass
+	# position of lower ball based on upper object
+	var x_2 = x_1 + rope_length_2 * sin(angle_2)
+	var y_2 = y_1 + rope_length_2 * cos(angle_2)
 	
+	# Update lower
+	lower_ball.position = Vector3(x_2, y_2, 0)
+	
+	print("Upper Ball Position:", upper_ball.position)
+	print("Lower Ball Position:", lower_ball.position)
