3c4a0205017d024be3122b2354ef34ce455e0ce8
Harjot <Harjot.Singh10@Student.ufv.ca>
2024-11-09 17:24:18 -0800

    Configuring damping to fix pendulum infinite motion

--- arm_1.gd
+++ arm_1.gd
@@ -6,7 +6,7 @@ var angle_velocity = 0.0  # Angular velocity
 var angle_acceleration = 0.0  # Angular acceleration
 var length = 5.0  # Length of the pendulum arm (matches the Y-size)
 var gravity = 9.81  # Acceleration due to gravity
-var damping = 0.995  # Damping factor to simulate air resistance
+var damping = 0.2# Damping factor to simulate air resistance
 
 func _physics_process(delta):
 	# Calculate angular acceleration
@@ -16,7 +16,7 @@ func _physics_process(delta):
 	angle_velocity += angle_acceleration * delta
 	
 	# Apply damping
-	angle_velocity *= damping
+	#angle_velocity *= damping
 	
 	# Update angle
 	angle += angle_velocity * delta
